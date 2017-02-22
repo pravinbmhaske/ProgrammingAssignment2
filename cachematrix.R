c99e5176b145ef6bdd800d63cbdd8ee2434bb232
Pravin B Mhaske <pravinmhaske@gmail.com>
2017-02-22 11:42:01 +0530

    Created functions
    makeCacheMatrix and cacheResolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix - This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                inv <- NULL
+                set <- function(y) {                     
+                        x <<- y                         ## y is input argument/matrix
+                        inv <<- NULL                    ## set inverse to NULL 
+                }
+                get <- function() x                     ## function to get the input matrix
+                setinverse <- function(inverse) inv <<- inverse ## function to calculate inverse of input matrix
+                getinverse <- function() inv            ## function to get the inverse
+                list(set = set, get = get,              ## return list of functions
+                     setinverse = setinverse,
+                     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve will retrieve the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {               ## takes makeCacheMatrix as input
+        inv <- x$getinverse()
+        if(!is.null(inv)) {                    ## check if inverse is already cached
+                message("getting cached data") 
+                return(inv)                    ## if yes, return it and end cacheSolve function        
+        }
+        data <- x$get()
+        inv <- solve(data, ...)                ## calculate inverse of input matrix
+        x$setinverse(inv)
+        inv                                    ## return inverse of matrix
 }
